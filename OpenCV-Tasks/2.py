2e8235d43fb83d91deabf2b606d267769347eb47
ASlugin <slugin.alexander@gmail.com>
2023-12-14 20:04:46 +0300

    Fix solution for 2 task

--- OpenCV-Tasks/2.py
+++ OpenCV-Tasks/2.py
@@ -1,22 +1,21 @@
 import cv2
 
-image_path = "input_images/photo1.jpg"
-output_image_path = "output_images/image-with-keypoints.jpg"
+image_path = "input_images/photo2.jpg"
+output_image_path = "output_images/image-with-key_points.jpg"
 
 # Loading the image
 img = cv2.imread(image_path)
 
-# Converting image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# # Converting image to grayscale
+# gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Applying SIFT detector
-sift = cv2.SIFT.create()
-kp = sift.detect(gray, None)
+sift = cv2.SIFT_create()
+key_points = sift.detect(img, None)
 
 # Marking the keypoint on the image using circles
-img = cv2.drawKeypoints(gray,
-                        kp,
-                        img,
-                        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+img = cv2.drawKeypoints(
+    img, key_points, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+)
 
 cv2.imwrite(output_image_path, img)
