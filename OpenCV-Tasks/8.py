f44ad1cb245248bf5f5cab37023384c789c7b791
ASlugin <slugin.alexander@gmail.com>
2023-12-14 22:33:48 +0300

    Add solution for 8 task

---
+++ OpenCV-Tasks/8.py
@@ -0,0 +1,16 @@
+import cv2
+
+image_path = "input_images/photo1.jpg"
+output_image_path = "output_images/rotated_by_45_degrees.jpg"
+
+img = cv2.imread(image_path)
+
+# find height, width and center of image
+height, width = img.shape[:2]
+center_of_image = (int(width / 2), int(height / 2))
+
+# Rotate by 45 degrees and reduce the image size by 0.6 to fit in image
+rotation_matrix = cv2.getRotationMatrix2D(center_of_image, -45, 0.6)
+rotated = cv2.warpAffine(img, rotation_matrix, (width, height))
+
+cv2.imwrite(output_image_path, rotated)
